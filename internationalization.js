c8ce7f2c553008e546bbbd2df1519123b2d861bc
erosman <erosman@users.noreply.github.com>
2017-09-27 19:28:30 +0330

    Update internationalization.js

--- internationalization.js
+++ internationalization.js
@@ -1,9 +1,7 @@
 // ----------------- Internationalization ------------------
-(() => { // anonymous self-invoking function to limit the scope
-  for (let node of document.querySelectorAll('[data-i18n]')) {
-    let [text, attr] = node.dataset.i18n.split('|');
-    text = chrome.i18n.getMessage(text);
-    attr ? node[attr] = text : node.appendChild(document.createTextNode(text));
-  }
-})();
+for (let node of document.querySelectorAll('[data-i18n]')) {
+  let [text, attr] = node.dataset.i18n.split('|');
+  text = chrome.i18n.getMessage(text);
+  attr ? node[attr] = text : node.appendChild(document.createTextNode(text));
+}
 // ----------------- /Internationalization -----------------
