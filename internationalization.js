16cd5873b89e4341ee4eb81e912ab435b856fe19
Aaron Papp <Aaron-P@users.noreply.github.com>
2021-10-24 23:54:10 +0000

    Replace attribute indexer with setAttribute

--- internationalization.js
+++ internationalization.js
@@ -2,6 +2,6 @@
 for (const node of document.querySelectorAll('[data-i18n]')) {
   let [text, attr] = node.dataset.i18n.split('|');
   text = chrome.i18n.getMessage(text);
-  attr ? node[attr] = text : node.appendChild(document.createTextNode(text));
+  attr ? node.setAttribute(attr, text) : node.appendChild(document.createTextNode(text));
 }
 // ----------------- /Internationalization -----------------
